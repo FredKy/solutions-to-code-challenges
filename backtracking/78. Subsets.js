88c4d5793beaa16acb012895c3ba0032c0e749a9
FredKy <pinb4llwiz@protonmail.com>
2023-05-31 09:32:38 +0200

    Compute powerset

---
+++ backtracking/78. Subsets.js
@@ -0,0 +1,21 @@
+var subsets = function (nums) {
+  var res = [];
+
+  var subset = [];
+  function depthFirstSearch(i) {
+    if (i > nums.length - 1) {
+      res.push([...subset]);
+      return;
+    }
+    // Decision to include element in subset
+    subset.push(nums[i]);
+    depthFirstSearch(i + 1);
+
+    // Decision to NOT include element
+    subset.pop();
+    depthFirstSearch(i + 1);
+  }
+
+  depthFirstSearch(0);
+  return res;
+};
