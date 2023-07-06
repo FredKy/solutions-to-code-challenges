7ab7e63487587c1dcd4670e9d9a7cccff1e99012
FredKy <pinb4llwiz@protonmail.com>
2023-07-06 19:37:22 +0200

    Readded removed solution

--- hackerrank/3or5sum.js
+++ hackerrank/3or5sum.js
@@ -10,6 +10,18 @@ function sumAnswer(n) {
   console.log(sum);
 }
 
+function sumBigInt(n) {
+  const N = BigInt(n);
+  const numberOfThrees = (N - 1n) / 3n;
+  const numberOfFives = (N - 1n) / 5n;
+  const numberOfFifteens = (N - 1n) / 15n;
+  const threeSum = ((numberOfThrees * (numberOfThrees + 1n)) / 2n) * 3n;
+  const fiveSum = ((numberOfFives * (numberOfFives + 1n)) / 2n) * 5n;
+  const fifteenSum = ((numberOfFifteens * (numberOfFifteens + 1n)) / 2n) * 15n;
+
+  console.log((threeSum + fiveSum - fifteenSum).toString());
+}
+
 function sum(n) {
   const N = b.BigNumber(n - 1);
   const numberOfThrees = N.dividedToIntegerBy(3);
@@ -34,6 +46,9 @@ function sum(n) {
 sum(2);
 sum(10);
 sum(100);
+sumBigInt(2);
+sumBigInt(10);
+sumBigInt(100);
 sumAnswer(2);
 sumAnswer(10);
 sumAnswer(100);
