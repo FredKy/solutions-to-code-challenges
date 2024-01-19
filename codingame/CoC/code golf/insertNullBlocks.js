0f9616c676172d3c36e3d2e4a3e5d2a9670d138d
FredKy <pinb4llwiz@protonmail.com>
2024-01-19 10:42:00 +0100

    Ruby CG solution

--- codingame/CoC/code golf/insertNullBlocks.js
+++ codingame/CoC/code golf/insertNullBlocks.js
@@ -1,2 +1,10 @@
 b=+(r=readline)(d=r(s=0))
 while(b--){[p,l]=r().split` `;d=d.slice(0,+p+s)+".".repeat(+l)+d.slice(+p+s);s+=+l}print(d)
+
+//ruby solution
+/* 
+w=gets
+gets
+$<.reverse_each{|x|a=x.split.map(&:to_i);w.insert(a[0],"."*a[1])}
+$><<w
+*/
